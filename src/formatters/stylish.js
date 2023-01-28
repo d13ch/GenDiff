a2e2ba596e1c363ca036e8817b7beba1936144f6
Dmitrii Chernov <dm.a.chernov@gmail.com>
2023-01-28 15:09:09 +0300

    fix stylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -2,21 +2,19 @@ import _ from 'lodash';
 import generateDiffTree from '../index.js';
 import parse from '../parser.js';
 
-const stringify = (data, spacesCount) => {
+const ident = (level = 1, spacesCount = 1, correction = 0) => ' '.repeat(level * spacesCount - correction);
+
+const stringify = (data, initialDepth) => {
   const iter = (currentValue, depth) => {
     if (!_.isObject(currentValue)) {
       return `${currentValue}`;
     }
     const entries = Object.entries(currentValue);
-    const spaces = '    ';
-    const identSize = depth * spacesCount;
-    const currentIndent = spaces.repeat(identSize);
-    const strings = entries.map(([key, value]) => `${currentIndent}${key}: ${iter(value, depth + 1)}`);
+    const strings = entries.map(([key, value]) => `${ident(depth, 4)}${key}: ${iter(value, depth + 1)}`);
 
-    return `{\n${strings.join('\n')}\n${spaces.repeat(identSize - depth)}}`;
+    return `{\n${strings.join('\n')}\n${ident(depth, 4, 4)}}`;
   };
-
-  return iter(data, 1);
+  return iter(data, initialDepth);
 };
 
 const generateStylishDiff = (filePath1, filePath2) => {
@@ -25,25 +23,24 @@ const generateStylishDiff = (filePath1, filePath2) => {
   const diffTree = generateDiffTree(fileData1, fileData2);
 
   const iter = (tree, depth) => {
-    const braceIdent = (level) => ' '.repeat(level * 4 - 4);
-    const ident = (level) => ' '.repeat(level * 4 - 2);
+    const currentIdent = ident(depth, 4, 2);
     const result = tree.map((node) => {
       const strValue = (value = node.value) => stringify(value, depth + 1);
       switch (node.tag) {
         case 'deleted':
-          return `${ident(depth)}- ${node.key}: ${strValue()}`;
+          return `${currentIdent}- ${node.key}: ${strValue()}`;
         case 'added':
-          return `${ident(depth)}+ ${node.key}: ${strValue()}`;
+          return `${currentIdent}+ ${node.key}: ${strValue()}`;
         case 'changed':
-          return `${ident(depth)}- ${node.key}: ${strValue(node.value.value1)}\n${ident(depth)}+ ${node.key}: ${strValue(node.value.value2)}`;
+          return `${currentIdent}- ${node.key}: ${strValue(node.value.value1)}\n${currentIdent}+ ${node.key}: ${strValue(node.value.value2)}`;
         case 'nested':
-          return `${ident(depth)}  ${node.key}: ${iter(node.value, depth + 1)}`;
+          return `${currentIdent}  ${node.key}: ${iter(node.value, depth + 1)}`;
         default:
-          return `${ident(depth)}  ${node.key}: ${strValue()}`;
+          return `${currentIdent}  ${node.key}: ${strValue()}`;
       }
     });
 
-    return `{\n${result.join('\n')}\n${braceIdent(depth)}}`;
+    return `{\n${result.join('\n')}\n${ident(depth, 4, 4)}}`;
   };
 
   return iter(diffTree, 1);
